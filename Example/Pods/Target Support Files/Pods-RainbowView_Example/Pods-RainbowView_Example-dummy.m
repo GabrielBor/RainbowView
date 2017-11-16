0417fa0cea1ee27b47bd318e72ea5223fbe81093
Gabriel Borges <gabrissousa@gmail.com>
2017-11-16 09:48:55 -0200

    create lib

---
+++ Example/Pods/Target Support Files/Pods-RainbowView_Example/Pods-RainbowView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RainbowView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RainbowView_Example
+@end
---
+++ Example/Pods/Target Support Files/RainbowView/RainbowView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RainbowView : NSObject
+@end
+@implementation PodsDummy_RainbowView
+@end
